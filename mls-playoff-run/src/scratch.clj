a2b5ec2108d4e2f9387dafa92bb0af5aaef1f48f
Nate Jones <nate@endot.org>
2024-08-27 19:28:07 -0700

    Take a look at remaining playoff games

--- mls-playoff-run/src/scratch.clj
+++ mls-playoff-run/src/scratch.clj
@@ -4,7 +4,10 @@
             [clojure.java.io :as io]
             [clojure.pprint :refer [pprint print-table]]
             [clojure.string :as str]
-            [org.httpkit.client :as http]))
+            [clojure.string :as string]
+            [org.httpkit.client :as http])
+  (:import (java.time ZonedDateTime ZoneId)
+           (java.time.format DateTimeFormatter)))
 
 (def api-key (System/getenv "API_FOOTBALL_API_KEY"))
 (def base-url "https://v3.football.api-sports.io/")
@@ -135,6 +138,85 @@
   (saveable? (cache-lookup (cache-key (teams-req 253 2022))))
   (print-table (sort-by :surface (map :venue (:response mls-teams))))
   (def status (cached-request (status-req))))
+
+(defn fixture-played-by?
+  [teams]
+  (fn [fixture]
+    (or (teams (-> fixture :teams :home :name))
+        (teams (-> fixture :teams :away :name)))))
+
+(defn fixture-status?
+  [status]
+  (fn [fixture]
+    (= status (-> fixture :fixture :status :short))))
+
+(defn fixture-round?
+  [round]
+  (fn [fixture]
+    (= round (-> fixture :league :round))))
+
+(defn fixture->opponent
+  [fixture for-whom]
+  (let [home-team (-> fixture :teams :home :name)
+        away-team (-> fixture :teams :away :name)]
+    (if (= home-team for-whom)
+      (str "vs " away-team)
+      (str "at " home-team))))
+
+(defn team-schedules
+  [rounds fixtures teams]
+  (for [round rounds
+        :let [round-fixture (first (filter (fixture-round? round) fixtures))]
+        ]
+    (reduce
+      (fn [result team]
+        (let [fixtures (->> fixtures
+                            (filter (fixture-played-by? #{team}))
+                            (filter (fixture-round? round))
+                            )
+              fixture (first fixtures)]
+          (assoc result team (some-> fixture (fixture->opponent team)))))
+      {:round round
+       :date (-> round-fixture :fixture :date #_#_(string/split #"T") first)}
+      teams)))
+
+(defn team-schedules2
+  [fixtures teams]
+  (->> (for [team teams
+             fixture (->> fixtures
+                          (filter (fixture-played-by? #{team}))
+                          (filter (fixture-status? "NS")))
+             :let [dt-str (-> fixture :fixture :date)
+                   dt (-> (ZonedDateTime/parse dt-str DateTimeFormatter/ISO_OFFSET_DATE_TIME)
+                          (.withZoneSameInstant (ZoneId/systemDefault)))]]
+         {:date (.format dt DateTimeFormatter/ISO_LOCAL_DATE)
+          team (some-> fixture (fixture->opponent team))})
+       (group-by :date)
+       vals
+       (map #(apply merge %))))
+
+
+(comment
+  (fixture->opponent (last (:response mls-fixtures)) "Seattle Sounders")
+  (map (comp :name :team) (:response mls-teams))
+  (->> (group-by (comp :round :league) (:response mls-fixtures))
+       (into {} (map (juxt first (comp count second)))))
+  (->> (:response mls-fixtures)
+       (filter (fixture-played-by? #{"Los Angeles Galaxy"}))
+       (filter (fixture-status? "FT"))
+       last)
+  (tap> (drop 35 (:response mls-rounds)))
+  (->> (team-schedules (drop 35 (:response mls-rounds))
+                       (:response mls-fixtures)
+                       ["Los Angeles Galaxy" "Los Angeles FC"])
+       (print-table [:round :date "Los Angeles Galaxy" "Los Angeles FC"]))
+  (let [teams ["Los Angeles Galaxy" "Los Angeles FC" "Inter Miami" "FC Cincinnati"]]
+    (->> (team-schedules2 (:response mls-fixtures) teams)
+         (sort-by :date)
+         (print-table (cons :date teams))))
+  (-> (ZonedDateTime/parse "2024-09-01T02:30:00+00:00" DateTimeFormatter/ISO_OFFSET_DATE_TIME)
+      (.withZoneSameInstant (ZoneId/systemDefault)))
+  )
 
 ; (into [] (range 100))
 
